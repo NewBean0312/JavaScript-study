ce96cb1046e4b3e9475c7c96a2df7cd108c2b301
NewBean0312 <newbean0312@gmail.com>
2024-03-30 13:06:12 +0900

    2.문법 1)주석

---
+++ BASIC 자바스크립트/기본 문법/주석.js
@@ -0,0 +1,15 @@
+// 기본 한 줄 주석
+
+/* 
+    여러 줄 
+    주석
+*/
+
+// JS DOC : JavaScript API 문서 생성기
+/**
+ * Represents a book.
+ * @constructor
+ * 
+ * @param {string} title - The title of the book.
+ * @param {string} author = The author of the book.
+ */
